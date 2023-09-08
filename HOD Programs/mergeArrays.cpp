0b1a0b73fc432f7ea95e4f4409658f7bfa7599c3
Hamza Mehmood <hamzamehmood167@gmail.com>
2023-09-08 23:31:50 +0500

    Just Minor Changes to merge Arrays Program

--- HOD Programs/mergeArrays.cpp
+++ HOD Programs/mergeArrays.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 int *mergeArrays(int *, int, int *, int);
-int *mergeArraysWithoutDup(int *, int, int *, int);
 void print(int *, int);
 
 int main()
@@ -11,10 +10,8 @@ int main()
     int arr2[5]{1, 5, 6, 7, 8};
     int *arr3;
     arr3 = mergeArrays(arr1, 5, arr2, 5);
-    int *arr4 = mergeArraysWithoutDup(arr1, 5, arr2, 5);
 
     print(arr3, 10);
-    print(arr4, 10);
     return 0;
 }
 
@@ -42,34 +39,6 @@ int *mergeArrays(int *first, int size1, int *second, int size2)
         {
             third[k++] = first[i++];
             third[k++] = second[j++];
-        }
-    }
-    return third;
-}
-
-int *mergeArraysWithoutDup(int *first, int size1, int *second, int size2)
-{
-    int newSize{size1 + size2};
-
-    int *third = new int[newSize];
-
-    int i{}, j{}, k{};
-
-    while (k < newSize)
-    {
-        if (first[i] > second[j])
-        {
-            third[k++] = second[j++];
-        }
-        else if (first[i] < second[j])
-        {
-            third[k++] = first[i++];
-        }
-        else
-        {
-            third[k++] = first[i++];
-            // third[k++] = second[j++];
-            j++;
         }
     }
     return third;
