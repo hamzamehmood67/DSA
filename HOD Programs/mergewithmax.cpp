0d509628acec0738b31d118b5afb4428d805c296
Hamza Mehmood <hamzamehmood167@hotmail.com>
2023-09-25 10:15:02 +0500

    newe version

--- HOD Programs/mergewithmax.cpp
+++ HOD Programs/mergewithmax.cpp
@@ -6,43 +6,54 @@ void print(int *, int);
 
 int main()
 {
-    int arr1[5]{1, 3, 4, 8, 9};
-    int arr2[3]{5, 5, 5};
-    int *arr3;
-    arr3 = mergeArrays(arr1, 5, arr2, 3);
+    int arr1[]{5, 5, 5, 6};
+    int size1 = sizeof(arr1) / sizeof(arr1[0]);
 
-    print(arr3, 8);
+    int arr2[]{1, 3, 4, 6};
+    int size2 = sizeof(arr2) / sizeof(arr2[0]);
+    int *arr3;
+    arr3 = mergeArrays(arr1, size1, arr2, size2);
+    int size3 = *(&arr3 + 1) - arr3;
+    print(arr3, 9);
     return 0;
 }
 
-/// Fucntions/////////
-
 int *mergeArrays(int *first, int size1, int *second, int size2)
 {
-    int newSize{size1 + size2};
-
+    int newSize = size1 + size2;
     int *third = new int[newSize];
-
-    int i{}, j{}, k{};
+    int i = 0, j = 0, k = 0;
 
     while (i < size1 && j < size2)
     {
         if (first[i] < second[j])
+        {
             third[k++] = first[i++];
-
-        else
+        }
+        else if (first[i] > second[j])
+        {
             third[k++] = second[j++];
+        }
+        else
+        {
+            third[k++] = first[i++];
+            j++;
+        }
     }
 
+    // Copy remaining elements from the non-empty array
+
     while (i < size1)
+    {
         third[k++] = first[i++];
+    }
 
     while (j < size2)
+    {
         third[k++] = second[j++];
-
+    }
     return third;
 }
-
 void print(int *arr, int size)
 {
     for (int i = 0; i < size; i++)
