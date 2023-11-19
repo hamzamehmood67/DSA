6bae27559d722a220a333768b0e89e8d02868f9b
Hamza Mehmood <thehamzamehmood@gmail.com>
2023-11-19 23:47:43 +0500

    Testing cross directories .h file

--- Stack/Balance_para/main2.cpp
+++ Stack/Balance_para/main2.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-#include <stack>
-#include <cstring>
+#include "../Stack_Array_Implementation/stack.h"
 
 using namespace std;
 
 int main()
 {
+    STACK obj(5);
+
+    obj.push(5);
+    obj.push(3);
+
+    obj.display();
 }
